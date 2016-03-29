2a00113bb87b84daafa0838e58fd497fcdbd0118
Kadrov Maxim <kadrovmax@gmail.com>
2016-03-29 09:40:04 +0300

    upp

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
 	$conn = mysql_connect('localhost','root','root');
-	$db = mysql_select_db('database_for_phones')
+	$db = mysql_select_db('database_for_phones');
+	/////////////////////////////
 
 
 
