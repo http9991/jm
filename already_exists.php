f358ef42add4baae263ccd68e847654becf46986
Kadrov Maxim <kadrovmax@gmail.com>
2016-03-29 09:26:30 +0300

    test commit

--- already_exists.php
+++ already_exists.php
@@ -1,8 +1,6 @@
 ﻿<?php
  		include_once('db.php');
-               
 		$username = $_POST['username'];
-		
 		$query= mysql_query("SELECT username FROM `ofuser` WHERE username = '$username'");
 		$results = mysql_fetch_array($query);
 		print_r($results['username']);
